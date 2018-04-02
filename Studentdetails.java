82a51d98a2cef894a92c69faf74d2ba909e91ba5
Jason Lamb <jrl1547@g.rit.edu>
2018-04-02 12:43:12 -0400

    update 4/2 2

---
+++ Studentdetails.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+
+public class Studentdetails{
+  
+   String username;
+   String mastersstart;
+   String capstonestart;
+   Database capstone_project;
+  
+   /**********************************************************************************
+   *                                   CONSTRUCTORS                                  *
+   **********************************************************************************/
+  
+  
+   public Studentdetails(){
+      capstone_project = new Database();
+   }
+   
+   public Studentdetails(String _username){
+      capstone_project = new Database();   
+   }
+   
+   public Studentdetails(String _username, String mastersstart, String capstonestart){
+      capstone_project = new Database();
+   }
+   
+   /**********************************************************************************
+   *                                   ACCESSORS                                     *
+   **********************************************************************************/
+   
+   
+   
+   /**********************************************************************************
+   *                                   MUTATORS                                      *
+   **********************************************************************************/
+   
+   
+   
+   /**********************************************************************************
+   *                                   METHODS                                       *
+   **********************************************************************************/
+  
+  
+
+
+}
