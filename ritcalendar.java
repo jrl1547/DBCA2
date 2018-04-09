cb599f8347ffd459f1272fc31bbbceff6d5c74a7
jrl1547 <36043175+jrl1547@users.noreply.github.com>
2018-04-09 07:37:22 -0400

    updates 4/9

--- ritcalendar.java
+++ ritcalendar.java
@@ -1,113 +1,136 @@
 import java.util.*;
-import java.sql.*;
 
 public class RITCalendar{
-   int term;
-   String startDate;
-   String addDropDeadline;
-   String gradeDeadline;
-   String endDate;
-   Database db = new Database();
-      
-   public RITCalendar(){
-   }
-   
-   public RITCalendar(int term){
-      this.term = term;
-   }
-   
-   public String getStartDate(){
-      return startDate;
-   }
-   
-   public void setStartDate(String startDate){
-      this.startDate = startDate;
-   }
-   
-   public String getAddDropDeadline(){
-      return this.addDropDeadline;
-   }
-   
-   public void setAddDropDeadline(String addDropDeadline){
-      this.addDropDeadline = addDropDeadline;
-   }
-   
-   public String getGradeDeadline(){
-      return this.gradeDeadline;
-   }
-   
-   public void setGradeDeadline(String gradeDeadline){
-      this.gradeDeadline = gradeDeadline;
-   }
-   
-   public String getEndDate(){
-      return this.endDate;
-   }
-   
-   public void setEndDate(String endDate){
-      this.endDate = endDate;
-   }
-   
-   /*
-      Fetch gets the data regarding the rit calendar for the term that was given in the constructor.
-   */
-   public boolean fetch(){
-      String select = "SELECT * FROM ritcalendar WHERE term = ?";
-      ArrayList<String> args = new ArrayList<String>();
-      args.add(""+this.term);
-      ArrayList<ArrayList<String>> fetchData = db.getData(select, args);
-      fetchData.remove(0);
-      this.term            = fetchData.get(0).get(0).toString();
-      this.startDate       = fetchData.get(0).get(1).toString();
-      this.addDropDeadline = fetchData.get(0).get(2).toString();
-      this.gradeDeadline   = fetchData.get(0).get(3).toString();
-      this.endDate         = fetchData.get(0).get(4).toString();
-   }
-   
-   /*
-      update data for a currently existing term in the ritcalendar
-   */
-   public int put(){
-      String update = "UPDATE ritcalendar SET startDate = ?, addDropDeadline = ?, gradeDeadline = ?, endDate = ? WHERE term = ?;";
-      ArrayList<String> args = new ArrayList<String>();
-      args.add(this.startDate);
-      args.add(this.addDropDeadline);
-      args.add(this.gradeDeadline);
-      args.add(this.endDate);
-      args.add(this.term);
-      if(db.setData(update, args)){
-         return 1;//success
-      }
-      return 0;//failure
-   }
-   
-   /*
-      insert all of the associated data into the database
-   */
-   public int post(){
-      String insert = "INSERT INTO ritcalendar SET(term, startDate, addDropDeadline, gradeDeadline, endDate) VALUES(?,?,?,?,?);";
-      ArrayList<String> args = new ArrayList<String>();
-      args.add(this.term);
-      args.add(this.startDate);
-      args.add(this.addDropDeadline);
-      args.add(this.gradeDeadline);
-      args.add(this.endDate);
-      if(db.setData(insert, args)){
-         return 1;//success
-      }
-      return 0;//failure
-   }
-   
-   /*
-      delete all calendar data associated with the term given
-   */
-   public int delete(){
-      String delete = "DELETE FROM ritcalendar WHERE term = ?;";
-      ArrayList<String> args = new ArrayList<String>();
-      args.add(ths.term);
-      if(db.setData(delete, args)){
-         return 1;//success
-      }
-      return 0;//failure
-   }
+    String term;
+    String startDate;
+    String addDropDeadline;
+    String gradeDeadline;
+    String endDate;
+    Database capstone_project;
+
+    /**********************************************************************************
+     *                                   CONSTRUCTORS                                  *
+     **********************************************************************************/
+    public RITCalendar(){
+        capstone_project = new Database();
+    }
+
+    public RITCalendar(String term){
+        capstone_project = new Database();
+        this.term = term;
+    }
+
+    public RITCalendar(String _term, String _startDate, String _addDropDeadline, String _gradeDeadline, String _endDate) {
+        capstone_project = new Database();
+        term = _term;
+        startDate = _startDate;
+        addDropDeadline = _addDropDeadline;
+        gradeDeadline = _gradeDeadline;
+        endDate = _endDate;
+    }
+
+
+    /**********************************************************************************
+     *                                   ACCESSORS                                     *
+     **********************************************************************************/
+    public String getStartDate(){
+        return startDate;
+    }
+
+    public String getAddDropDeadline(){
+        return this.addDropDeadline;
+    }
+
+    public String getGradeDeadline(){
+        return this.gradeDeadline;
+    }
+
+    public String getEndDate(){
+        return this.endDate;
+    }
+
+
+    /**********************************************************************************
+     *                                   MUTATORS                                      *
+     **********************************************************************************/
+    public void setStartDate(String startDate){
+        this.startDate = startDate;
+    }
+
+    public void setAddDropDeadline(String addDropDeadline){
+        this.addDropDeadline = addDropDeadline;
+    }
+
+    public void setGradeDeadline(String gradeDeadline){
+        this.gradeDeadline = gradeDeadline;
+    }
+
+    public void setEndDate(String endDate){
+        this.endDate = endDate;
+    }
+
+
+    /**********************************************************************************
+     *                                   METHODS                                       *
+     **********************************************************************************/
+    /*
+       Fetch gets the data regarding the rit calendar for the term that was given in the constructor.
+    */
+    public ArrayList<ArrayList<String>> fetch(String _term){
+        ArrayList<String> args = new ArrayList<String>();
+        args.add(_term);
+        String select = "SELECT * from ritcalendar WHERE _capstoneid = ?";
+        ArrayList<ArrayList<String>> fetchData = capstone_project.getData(select, args);
+        fetchData.remove(0);
+
+        term            = fetchData.get(0).get(0).toString();
+        startDate       = fetchData.get(0).get(1).toString();
+        addDropDeadline = fetchData.get(0).get(2).toString();
+        gradeDeadline   = fetchData.get(0).get(3).toString();
+        endDate         = fetchData.get(0).get(4).toString();
+
+        return fetchData;
+    }
+
+    /*
+       update data for a currently existing term in the ritcalendar
+    */
+    public boolean put(){
+        String update = "UPDATE ritcalendar SET startDate = ?, addDropDeadline = ?, gradeDeadline = ?, endDate = ? WHERE term = ?;";
+        ArrayList<String> args = new ArrayList<String>();
+        args.add(startDate);
+        args.add(addDropDeadline);
+        args.add(gradeDeadline);
+        args.add(endDate);
+        args.add(term);
+        boolean put = capstone_project.setData(update, args);
+        return put;
+    }
+
+    /*
+       insert all of the associated data into the database
+    */
+    public boolean post(){
+        String insert = "INSERT INTO ritcalendar SET(term, startDate, addDropDeadline, gradeDeadline, endDate) VALUES(?,?,?,?,?);";
+        ArrayList<String> args = new ArrayList<String>();
+        args.add(term);
+        args.add(startDate);
+        args.add(addDropDeadline);
+        args.add(gradeDeadline);
+        args.add(endDate);
+        boolean post = capstone_project.setData(insert, args);
+        return post;
+    }
+
+    /*
+       delete all calendar data associated with the term given
+    */
+    public boolean delete(){
+        String delete = "DELETE FROM ritcalendar WHERE term = ?;";
+        ArrayList<String> args = new ArrayList<String>();
+        args.add(term);
+        boolean del = capstone_project.setData(delete, args);
+        return del;
+    }
+
 }
